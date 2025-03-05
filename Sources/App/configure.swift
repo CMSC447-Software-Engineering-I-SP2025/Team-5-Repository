73c3c1c789a787c43923882ef3d48c8620ee4cdb
apocolipse <apocolipse@gmail.com>
2025-03-05 12:07:50 -0500

    [MOV-5] Create Base API

---
+++ Sources/App/configure.swift
@@ -0,0 +1,28 @@
+import NIOSSL
+import Fluent
+import FluentPostgresDriver
+import Leaf
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) async throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "movies_user",
+        password: Environment.get("DATABASE_PASSWORD") ?? "movies",
+        database: Environment.get("DATABASE_NAME") ?? "movies_database",
+        tls: .disable)
+    ), as: .psql)
+
+    app.migrations.add(CreateMovie())
+
+    app.views.use(.leaf)
+
+
+    // register routes
+    try routes(app)
+}
